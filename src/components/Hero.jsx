48024e286665bba22b1c19617aa37344570d6957
Mohamad javad samadi <javadsamadi255@gmail.com>
2025-04-19 18:12:12 +0330

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className=' relative h-dvh w-screen overflow-x-hidden'>
+      <div id='video-frame' className='relative z-10 h-dvh overflow-hidden w-screen  rounded-lg bg-blue-75'>
+        <div>
+            <div className="mask-clip-path">
+
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        zentry: ['zentry', 'sanf-serif'],
+        general:['general', 'sanf-serif'],
+        'circular-web': ['circular-web', 'sanf-serif'],
+        'robert-medium': ['robert-medium', 'sanf-serif'],
+        'robert-regular': ['robert-regular', 'sanf-serif'],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
