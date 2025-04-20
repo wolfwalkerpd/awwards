4cb16d4ba8e9c0ac3d20ec80fb4b57816561bcf5
Mohamad javad samadi <javadsamadi255@gmail.com>
2025-04-20 19:33:24 +0330

    sec commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 const Hero = () => {
-  return (
-    <div className=' relative h-dvh w-screen overflow-x-hidden'>
-      <div id='video-frame' className='relative z-10 h-dvh overflow-hidden w-screen  rounded-lg bg-blue-75'>
-        <div>
-            <div className="mask-clip-path">
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
 
+  const totalVideos = 3;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className=" relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh overflow-hidden w-screen  rounded-lg bg-blue-75">
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(currentIndex + 1)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
             </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
