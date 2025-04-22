53dbaf61ab1ab92bc635a04d5292d546f02d8cd1
Mohamad javad samadi <javadsamadi255@gmail.com>
2025-04-22 20:04:24 +0330

    4th commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const nextVideoRef = useRef(null);
 
   const handleVideoLoad = () => {
@@ -24,28 +28,71 @@ const Hero = () => {
     setCurrentIndex(upcomingVideoIndex);
   };
 
-  useGSAP(()=>{
-    if(hasClicked){
-      gsap.set('#next-video', {visibility: 'visible'})
-
-      gsap.to('3next-video', {
-        transformOrigin: 'center center',
-        scale: 1,
-        width: '100%',
-        height: '100%',
-        duration: '1',
-        ease: 'power1.inOut',
-        onStart: () => nextVideoRef.current.play();
-      })
-
-      gsap.from('#current')
+  useEffect(() => {
+    if(loadedVideos === totalVideos -1){
+      setIsLoading(false);
     }
-  }, {dependencies :[currentIndex], revertOnUpdate: true})
+  } ,[loadedVideos])
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: "1",
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1 inOut",
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className=" relative h-dvh w-screen overflow-x-hidden">
+
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
         className="relative z-10 h-dvh overflow-hidden w-screen  rounded-lg bg-blue-75">
@@ -110,8 +157,8 @@ const Hero = () => {
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-          G<b>a</b>ming
-        </h1>
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };
