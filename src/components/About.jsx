0f417dd24576436a29cac1b2cb4bcc4b22f047b6
Mohamad javad samadi <javadsamadi255@gmail.com>
2025-06-22 11:22:41 +0330

    forth commit

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id="about" className='min-h-screen w-screen'>
+        <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+            <h2 className='font-general text-sm uppercase md:text-[10px]'>
+                welcome to Zentry
+            </h2>
+
+            <div className='mt=5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+                Discover the world's largest share and adventure    
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
