d45ffc5f4ff3aead4c5bb55fd5a48e16ebaf5eb8
Mohamad javad samadi <javadsamadi255@gmail.com>
2025-06-23 20:04:38 +0330

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Features from './components/Features'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   )
 }
