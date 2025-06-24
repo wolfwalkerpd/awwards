256995c9239075e548bd1ca5dc804a1238beaba8
Mohamad javad samadi <javadsamadi255@gmail.com>
2025-06-24 09:09:43 +0330

    seventh commit

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import AnimatedTitle from './AnimatedTitle'
 
 const Story = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="min-h-dvh w-screen bg-black text-blue-50" id="story">
+        <div className="flex size-full flex-col items-center py-10 pb-24 ">
+            <p className='font-general text-sm uppercase md:text-[10px]'>the multiversal ip world</p>
+            <div className='relative size-full'>
+                <AnimatedTitle
+                title="The story of a hidden realm"
+                sectionId="#story"
+                className="mt-5 pointer-events-none mix-blend-difference relative z-10"
+                />
+                <div className='story-img-container'>
+                    <div className="story-img-mask">
+                        <div className="story-img-content">
+                            <img src="/img/entrance.webp" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
